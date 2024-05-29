c05b8d079772c640bcb6715f9102538cf3480af0
XingjianZhang <hsingchien.zhang@gmail.com>
2024-05-28 20:35:00 -0700

    perf

--- simple_joblist_0.sh
+++ simple_joblist_0.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-python multitask_classifier.py --use_gpu --gpuid 0 --loss_ratio 0.1 1 0.1 --batch_size 4 28 4 --optimizer hmpcgrad --prediction_out predictions/multi_hmpcgrad/
+python multitask_classifier.py --use_gpu --gpuid 0 --loss_ratio 1 0 0 --prediction_out predictions/msingle_sst/
