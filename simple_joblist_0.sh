6b771ea56c7f9989f18fb0d177a4f4cbf93cc0a1
XingjianZhang <hsingchien.zhang@gmail.com>
2024-05-28 21:10:10 -0700

    updated single sst model

--- simple_joblist_0.sh
+++ simple_joblist_0.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-python multitask_classifier.py --use_gpu --gpuid 0 --loss_ratio 1 0 0 --prediction_out predictions/msingle_sst/
+python multitask_classifier.py --use_gpu --gpuid 0 --loss_ratio 1 0 0 --prediction_out predictions/single_sst/
